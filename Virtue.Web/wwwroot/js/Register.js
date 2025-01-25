f4bab07b17da8bdaff0f08697e798532c70dc023
Sunny <im.singhsunny053@gmail.com>
2025-01-25 11:16:09 +0530

    Changes for register page

---
+++ Virtue.Web/wwwroot/js/Register.js
@@ -0,0 +1,48 @@
+﻿function validateForm() {
+    let isValid = true;
+
+    const firstName = $('#FirstName').val().trim();
+    const lastName = $('#LastName').val().trim();
+    const email = $('#Email').val().trim();
+    const password = $('#Password').val().trim();
+    const confirmPassword = $('#ConfirmPassword').val().trim();
+
+    if (firstName === '') {
+        $('#errorFirstName').text('First Name is required.');
+        isValid = false;
+    } else {
+        $('#errorFirstName').text('');
+    }
+
+    if (lastName === '') {
+        $('#errorLastName').text('Last Name is required.');
+        isValid = false;
+    } else {
+        $('#errorLastName').text('');
+    }
+
+    if (email === '') {
+        $('#errorEmail').text('Email is required.');
+        isValid = false;
+    } else if (!/^\S+\S+\.\S+$/.test(email)) {
+        $('#errorEmail').text('Invalid email format.');
+        isValid = false;
+    } else {
+        $('#errorEmail').text('');
+    }
+
+    if (password === '') {
+        $('#errorPassword').text('Password is required.');
+        isValid = false;
+    } else if (password !== confirmPassword) {
+        $('#errorConfirmPassword').text('Passwords do not match.');
+        isValid = false;
+    } else {
+        $('#errorPassword').text('');
+        $('#errorConfirmPassword').text('');
+    }
+
+    if (isValid) {
+        $('#registerForm').submit();
+    }
+}
